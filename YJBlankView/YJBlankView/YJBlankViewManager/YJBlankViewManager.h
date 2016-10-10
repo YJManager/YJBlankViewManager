015f14680d1b5af8be3d791b6a236a43b2a018c8
YJHou <8069060@qq.com>
2016-10-10 13:57:44 +0800

    add

---
+++ YJBlankView/YJBlankView/YJBlankViewManager/YJBlankViewManager.h
@@ -0,0 +1,13 @@
+//
+//  YJBlankViewManager.h
+//  YJBlankView
+//
+//  Created by YJHou on 16/10/10.
+//  Copyright © 2016年 YJHou. All rights reserved.
+//
+
+#ifndef YJBlankViewManager_h
+#define YJBlankViewManager_h
+
+
+#endif /* YJBlankViewManager_h */
